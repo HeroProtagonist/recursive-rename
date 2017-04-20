b01df3035e43b09d776e65d8af87b3982e29a035
heroprotagonist <jstepper91@gmail.com>
2017-04-20 00:55:12 -0400

    Implement basic folder traversal

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 import path from 'path'
 import Promise from 'bluebird'
+const argv = require('minimist')(process.argv.slice(2))
 const fs = Promise.promisifyAll(require('fs'))
 
-const recursiveRename = async () => {
-  const testFolder = '.'
-  const files = await fs.readdirAsync(testFolder)
+const exludePaths = new Set()
+
+exludePaths.add('node_modules')
+exludePaths.add('.git')
+
+const recursiveRename = async rootPath => {
+  const files = await fs.readdirAsync(rootPath)
 
   files.forEach(async file => {
-    const filePath = path.join(__dirname, file)
+    let filePath = makePath(rootPath, file)
 
     const stats = await fs.statAsync(filePath)
 
-    if (stats) console.log(file, stats.isDirectory())
+    if (stats.isDirectory() && !exludePaths.has(file)) {
+      console.log('Folder: ', file)
+      recursiveRename(filePath)
+    } else {
+      console.log('This file is: ', file)
+    }
 
   })
 }
 
+const makePath = (base = __dirname, file) => path.join(base, file)
 
-recursiveRename()
+const initialPath = '.'
+recursiveRename(initialPath)
