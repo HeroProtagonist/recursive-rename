02074c2c434d71a2e06c0c399630919278d083e0
heroprotagonist <jstepper91@gmail.com>
2017-05-01 21:48:58 -0400

    Export function and add babel runtime

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const argv = require('minimist')(process.argv.slice(2))
 import Traverser from './traverse'
 import excludes from './excludes'
 
-const initialPath = '.'
+function recursiveRename (initialPath) {
+  const traverser = new Traverser(excludes, initialPath)
+  traverser.traverse()
+}
 
-const traverser = new Traverser(excludes, initialPath)
-
-traverser.traverse()
+export default recursiveRename
