08d8bab53f982f1811ab58160a618b71ec01f308
heroprotagonist <jstepper91@gmail.com>
2017-05-06 15:12:43 -0400

    Add colors to logging

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,4 @@
+import colors from 'colors'
 import path from 'path'
 import Promise from 'bluebird'
 
@@ -18,10 +19,10 @@ class Traverser {
       const stats = await fs.statAsync(filePath)
 
       if (stats.isDirectory() && !this.excludes.has(file)) {
-        console.log('Folder: ', file)
+        console.log(`Folder:  ${file}`.red)
         this.traverse(filePath)
       } else {
-        console.log('This file is: ', file)
+        console.log(`This file is: ${file}`.green)
       }
     })
   }
