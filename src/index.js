7b98569f8d3c13ff3b366eb5dd4210d18c3b5d90
heroprotagonist <jstepper91@gmail.com>
2017-05-06 15:12:18 -0400

    Add hashbang

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const argv = require('minimist')(process.argv.slice(2))
+#! /usr/bin/env node
 
 import Traverser from './traverse'
+import Validator from './validator'
 import excludes from './excludes'
+
+const argv = require('minimist')(process.argv.slice(2))
+
+console.log(argv)
+const validator = new Validator(argv)
 
 function recursiveRename (initialPath) {
   const traverser = new Traverser(excludes, initialPath)
