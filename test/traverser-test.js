1f0c04305f002b14ddd33e1f35f1ffb169e9f117
heroprotagonist <jstepper91@gmail.com>
2017-05-08 00:08:33 -0400

    move mock definition into test body

--- test/traverser-test.js
+++ test/traverser-test.js
@@ -2,19 +2,18 @@ import expect from 'expect'
 import mock from 'mock-fs'
 import Traverser from '../src/traverser'
 
-
-mock({
-  'path/to/fake/dir': {
-    'some-file.txt': 'file content here',
-    'empty-dir': {/** empty directory */},
-  },
-  'path/to/some.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
-  'some/other/path': {/** another empty directory */},
-})
-
 describe('Traverse', () => {
 
   it('should exist', () => {
+
+    mock({
+      'path/to/fake/dir': {
+        'some-file.txt': 'file content here',
+        'empty-dir': {/** empty directory */},
+      },
+      'path/to/some.png': new Buffer([8, 6, 7, 5, 3, 0, 9]),
+      'some/other/path': {/** another empty directory */},
+    })
 
     const excludes = new Set()
 
@@ -27,12 +26,12 @@ describe('Traverse', () => {
     expect(traverser).toExist()
   })
 
-  after(() => {
-    mock.restore()
-  })
-
   it('should pass', () => {
     expect(true).toExist()
+  })
+
+  after(() => {
+    mock.restore()
   })
 
 })
