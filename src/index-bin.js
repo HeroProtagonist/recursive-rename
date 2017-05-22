0a9827c75891d3d18a0d70b19caeebbe603c16e3
heroprotagonist <jstepper91@gmail.com>
2017-05-22 19:24:19 -0400

    Initial work on cli

--- src/index-bin.js
+++ src/index-bin.js
@@ -1,11 +1,44 @@
 #! /usr/bin/env node
-import { version } from '../package.json'
+import { version as packageVersion } from '../package.json'
 import Traverser from './traverser'
 import Validator from './validator'
 import excludes from './excludes'
 
 const argv = require('minimist')(process.argv.slice(2))
 
-console.log(argv)
-console.log(version)
+const rename = args => {
+  const {
+    _,
+    dry,
+    exclude,
+    help,
+    override,
+    path,
+    version,
+  } = args
 
+  if (version) {
+    return console.log(packageVersion)
+  }
+
+  if (help) {
+    return console.log('TODO')
+  }
+  const src = _[0]
+  const dest = _[1]
+
+  const options = {
+    dry,
+    excludes,
+    override,
+    path,
+  }
+
+  console.log(exclude.split(','))
+
+  const validator = new Validator(src, dest, options)
+
+  return
+}
+
+rename(argv)
