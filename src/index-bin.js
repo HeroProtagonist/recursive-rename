c9b4187ebb25a7054d0764fccc2173951b9db9ea
heroprotagonist <jstepper91@gmail.com>
2017-05-23 17:53:16 -0400

    Continue command line work

--- src/index-bin.js
+++ src/index-bin.js
@@ -2,7 +2,16 @@
 import { version as packageVersion } from '../package.json'
 import Traverser from './traverser'
 import Validator from './validator'
-import excludes from './excludes'
+import Excludes from './excludes'
+
+// -h, --help
+// -d, --dry
+// -s, --src
+// -D?, --dest
+// -e, --exclude
+// -o, --override
+// -v, --version
+// -p, --path
 
 const argv = require('minimist')(process.argv.slice(2))
 
@@ -27,18 +36,34 @@ const rename = args => {
   const src = _[0]
   const dest = _[1]
 
+  const { excludes } = new Excludes(exclude)
+
   const options = {
-    dry,
     excludes,
     override,
-    path,
+    path, // validate path
   }
-
-  console.log(exclude.split(','))
 
   const validator = new Validator(src, dest, options)
 
-  return
+  return executeCommand(validator, dry)
+}
+
+const executeCommand = async (validator, dry) => {
+  const {
+    src,
+    dest,
+    excludes,
+    path,
+  } = validator
+
+  const traverser = new Traverser(path, {
+    src,
+    dest,
+    excludes,
+  })
+
+  await traverser.traverse({ dry })
 }
 
 rename(argv)
